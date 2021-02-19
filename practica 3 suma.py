7f15cf5e20d186976ad4e19508202e394aae9b0e
Saul Duran <saulduran@Cirujano09s-MacBook-Pro.local>
2021-02-18 21:57:13 -0400

    practica 3

---
+++ practica 3 suma.py
@@ -0,0 +1,17 @@
+print("Suma")
+a = 0
+b = 0
+while a and b !=1:
+    print("Suma")
+
+    print("Suma de numeros positivos")
+    a = int(input("Digite su primer numero positivo:\n"))
+    b = int(input("Digite su segundo numero positivo:\n"))
+
+    if a >= 0 and b >= 0:
+        print(a + b)
+    else:
+        print("Ese es un numero negativo")
+
+
+print("fin")
